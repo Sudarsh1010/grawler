7ee19c3a1ec796b997de7dfafa753f2e6ec71c84
Sudarsh1010 <sudarshjain1010@gmail.com>
2026-09-06 01:33:36 +0530

    refac: cleaner test extraction

--- internal/links/links_test.go
+++ internal/links/links_test.go
@@ -1,58 +1,48 @@
 package links
 
 import (
-	"slices"
+	"reflect"
 	"strings"
 	"testing"
 )
 
-func TestTracer(t *testing.T) {
+func TestExtract(t *testing.T) {
 	baseURL := "https://example.com"
-	want := []string{"https://example.com/about"}
 
-	s := `<a href="https://example.com/about">`
-	r := strings.NewReader(s)
+	tests := []struct {
+		name  string
+		input string
+		want  []string
+	}{
+		{
+			name:  "tracer",
+			input: `<a href="https://example.com/about">`,
+			want:  []string{"https://example.com/about"},
+		},
 
-	resp, err := Extract(r, baseURL)
-	if err != nil {
-		t.Error(err)
+		{
+			name:  "relative",
+			input: `<a href="/blog">`,
+			want:  []string{"https://example.com/blog"},
+		},
+		{
+			name:  "junks",
+			input: `<a href="https://example.com#about"><a href="mailto:a@b.c"><a>no href</a>`,
+			want:  []string{"https://example.com"},
+		},
 	}
 
-	if slices.Compare(resp, want) != 0 {
-		t.Errorf(`Extract(r, %s) = %q, %v, want match for %#q, nil`, baseURL, resp, err, want)
-	}
-}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := strings.NewReader(tt.input)
+			got, err := Extract(r, baseURL)
+			if err != nil {
+				t.Error(err)
+			}
 
-func TestResolveRelative(t *testing.T) {
-	baseURL := "https://example.com"
-	want := []string{"https://example.com/blog"}
-
-	s := `<a href="/blog">`
-	r := strings.NewReader(s)
-
-	resp, err := Extract(r, baseURL)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if slices.Compare(resp, want) != 0 {
-		t.Errorf(`Extract(r, %s) = %q, %v, want match for %#q, nil`, baseURL, resp, err, want)
-	}
-}
-
-func TestJunkIgnoreJunks(t *testing.T) {
-	baseURL := "https://example.com"
-	want := []string{"https://example.com"}
-
-	s := `<a href="https://example.com#about"><a href="mailto:a@b.c"><a>no href</a>`
-	r := strings.NewReader(s)
-
-	resp, err := Extract(r, baseURL)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if slices.Compare(resp, want) != 0 {
-		t.Errorf(`Extract(r, %s) = %q, %v, want match for %#q, nil`, baseURL, resp, err, want)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("got %v want %v", got, tt.want)
+			}
+		})
 	}
 }
